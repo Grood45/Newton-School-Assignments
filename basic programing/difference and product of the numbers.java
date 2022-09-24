a7a1cf839ebe48c8bee1fb4d00992dd5f6d2a999
MOHAMMAD SHER KHAN <101539547+Grood45@users.noreply.github.com>
2022-09-24 19:45:58 +0530

    first commit

---
+++ basic programing/difference and product of the numbers.java
@@ -0,0 +1,10 @@
+/*question ;-Write a program to print the difference and product of the numbers 45 and 32.*/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    // \n is used nextline
+		System.out.println("difference= " +(45-32));
+		System.out.println("product= "+(45*32));
+	}
+}
