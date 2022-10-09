77f3b28f1f69be0cd001c597cee411d37270e86e
MOHAMMAD SHER KHAN <101539547+Grood45@users.noreply.github.com>
2022-10-09 16:57:49 +0530

    first commit

---
+++ 1D Array/CHECK LENGTH OF ARRAY.java
@@ -0,0 +1,12 @@
+/*QUSETION- HOW TO CHECK LENGTH OF ARRAY */
+public class Main
+{
+	public static void main(String[] args) {
+		int[] a = {70,80,89,87,67,89,78,};
+		    
+		    System.out.println(a.length);
+		for(int i=0 ; i<a.length;i++)
+		System.out.println("index "+i+ " value = "+a[i]);
+		
+	}
+}
