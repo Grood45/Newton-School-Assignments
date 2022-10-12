7b09dd3c0c4a0a14c51514f266cc9932731ec2ab
MOHAMMAD SHER KHAN <101539547+Grood45@users.noreply.github.com>
2022-10-13 01:33:38 +0530

    Delete Newton School Arena assignment directory

--- Newton School Arena assignment/Basics of Programming/programing syntax/Number dilemma.java
+++
@@ -1,9 +0,0 @@
-static int StringToInt(String S){
-int i=Integer.parseInt(S);  
-return i;
-}
-
-static String IntToString(int N){
-String s=Integer.toString(N);
-return s;
-}
--- Newton School Arena assignment/Basics of Programming/programing syntax/String Typecast.java
+++
@@ -1,7 +0,0 @@
-static String checkConevrtion(int a)
- { 
-       
-       String str = Integer.toString(a);
-       return str ;
-      
-    }
